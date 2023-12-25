e1c7215ffae91a5e6980b5eb2cf913751846b6af
koonlookhin <krit_tipnuan@cmu.ac.th>
2023-12-25 17:24:24 +0700

    firebase

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,32 @@ function App() {
   const [isSensorActive, setIsSensorActive] = useState(false);
   const [acceleration, setAcceleration] = useState({ x: 0, y: 0, z: 0 });
   const [gyroscope, setGyroscope] = useState({ alpha: 0, beta: 0, gamma: 0 });
+ 
   useEffect(() => {
     let sensorEventListener;
-
+    // console.log("test")
     const startSensor = () => {
       sensorEventListener = (event) => {
+       if(event instanceof DeviceOrientationEvent){
+         console.log(event.alpha)
+         setGyroscope({
+          alpha: event.alpha.toFixed(2),
+          beta: event.beta.toFixed(2),
+          gamma: event.gamma.toFixed(2),
+        });
+       }
+        // console.log('Sensor Event:', event);
+        // console.log(event.ac)
         if (event.accelerationIncludingGravity) {
           const accelerationValues = event.accelerationIncludingGravity;
+          console.log("test"+acceleration.x)
           setAcceleration({
             x: accelerationValues.x.toFixed(2),
             y: accelerationValues.y.toFixed(2),
             z: accelerationValues.z.toFixed(2),
           });
         }
+        console.log("x: "+acceleration.x)
 
         if (event.rotationRate) {
           const gyroscopeValues = event.rotationRate;
@@ -48,14 +61,13 @@ function App() {
       stopSensor();
     };
   }, [isSensorActive]);
-
+  
   const toggleSensor = () => {
     setIsSensorActive((prevIsSensorActive) => !prevIsSensorActive);
   };
-
   return (
     <>
-    <div>
+   <div>
       <h1>Accelerometer and Gyroscope Example</h1>
       <p>Acceleration: X: {acceleration.x}, Y: {acceleration.y}, Z: {acceleration.z}</p>
       <p>Gyroscope: Alpha: {gyroscope.alpha}, Beta: {gyroscope.beta}, Gamma: {gyroscope.gamma}</p>
