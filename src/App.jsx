6e2b23c9a1856b5cb4bfe57f276f5d68aa915afd
koonlookhin <krit_tipnuan@cmu.ac.th>
2023-12-25 10:24:48 +0700

    add some content to test

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,68 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isSensorActive, setIsSensorActive] = useState(false);
+  const [acceleration, setAcceleration] = useState({ x: 0, y: 0, z: 0 });
+  const [gyroscope, setGyroscope] = useState({ alpha: 0, beta: 0, gamma: 0 });
+  useEffect(() => {
+    let sensorEventListener;
+
+    const startSensor = () => {
+      sensorEventListener = (event) => {
+        if (event.accelerationIncludingGravity) {
+          const accelerationValues = event.accelerationIncludingGravity;
+          setAcceleration({
+            x: accelerationValues.x.toFixed(2),
+            y: accelerationValues.y.toFixed(2),
+            z: accelerationValues.z.toFixed(2),
+          });
+        }
+
+        if (event.rotationRate) {
+          const gyroscopeValues = event.rotationRate;
+          setGyroscope({
+            alpha: gyroscopeValues.alpha.toFixed(2),
+            beta: gyroscopeValues.beta.toFixed(2),
+            gamma: gyroscopeValues.gamma.toFixed(2),
+          });
+        }
+      };
+
+      window.addEventListener('deviceorientation', sensorEventListener);
+    };
+
+    const stopSensor = () => {
+      window.removeEventListener('deviceorientation', sensorEventListener);
+    };
+
+    if (isSensorActive) {
+      startSensor();
+    } else {
+      stopSensor();
+    }
+
+    return () => {
+      stopSensor();
+    };
+  }, [isSensorActive]);
+
+  const toggleSensor = () => {
+    setIsSensorActive((prevIsSensorActive) => !prevIsSensorActive);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h1>Accelerometer and Gyroscope Example</h1>
+      <p>Acceleration: X: {acceleration.x}, Y: {acceleration.y}, Z: {acceleration.z}</p>
+      <p>Gyroscope: Alpha: {gyroscope.alpha}, Beta: {gyroscope.beta}, Gamma: {gyroscope.gamma}</p>
+      <button onClick={toggleSensor}>
+        {isSensorActive ? 'Stop Sensor' : 'Start Sensor'}
+      </button>
+    </div>
     </>
   )
 }
