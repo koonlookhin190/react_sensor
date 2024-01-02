c51fa64b8a156e4a7fff809343c36af63673ddaa
koonlookhin <krit_tipnuan@cmu.ac.th>
2024-01-02 17:34:40 +0700

    add style and noti

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import playIcon from "./assets/play.png";
+import pauseIcon from "./assets/pause.png";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 function App() {
   const [isSensorActive, setIsSensorActive] = useState(false);
   const [acceleration, setAcceleration] = useState({ x: 0, y: 0, z: 0 });
   const [gyroscope, setGyroscope] = useState({ alpha: 0, beta: 0, gamma: 0 });
-
+  const [holdTimeout, setHoldTimeout] = useState(null);
   useEffect(() => {
     let sensorEventListener;
 
@@ -22,7 +26,10 @@ function App() {
             gamma: event.rotationRate.gamma.toFixed(2),
           });
         }
-        if (event.accelerationIncludingGravity instanceof DeviceMotionEventAcceleration) {
+        if (
+          event.accelerationIncludingGravity instanceof
+          DeviceMotionEventAcceleration
+        ) {
           console.log("Asscelor");
           setAcceleration({
             x: event.accelerationIncludingGravity.x.toFixed(2),
@@ -30,27 +37,19 @@ function App() {
             z: event.accelerationIncludingGravity.z.toFixed(2),
           });
         }
-        // console.log('Sensor Event:', event);
-        // console.log(event.ac)
-        // if (event.accelerationIncludingGravity) {
-        //   const accelerationValues = event.accelerationIncludingGravity;
-        //   console.log("test"+acceleration.x)
-        //   setAcceleration({
-        //     x: accelerationValues.x,
-        //     y: accelerationValues.y,
-        //     z: accelerationValues.z,
-        //   });
-        // }
-        // console.log("x: "+acceleration.x)
+        const shakeThreshold = 15;
+        const isShaking =
+          Math.abs(event.accelerationIncludingGravity.x) > shakeThreshold ||
+          Math.abs(event.accelerationIncludingGravity.y) > shakeThreshold ||
+          Math.abs(event.accelerationIncludingGravity.z) > shakeThreshold;
 
-        // if (event.rotationRate) {
-        //   const gyroscopeValues = event.rotationRate;
-        //   setGyroscope({
-        //     alpha: gyroscopeValues.alpha.toFixed(2),
-        //     beta: gyroscopeValues.beta.toFixed(2),
-        //     gamma: gyroscopeValues.gamma.toFixed(2),
-        //   });
+        // if (isShaking && holdTimeout !== null) {
+        //     clearTimeout(holdTimeout); 
+        //     showNotification('Device is shaking!');
         // }
+        if (isShaking) {
+          showNotification('Device is shaking!');
+         }
       };
 
       window.addEventListener("devicemotion", sensorEventListener);
@@ -60,26 +59,49 @@ function App() {
       window.removeEventListener("devicemotion", sensorEventListener);
     };
 
-    if (isSensorActive) {
+    // if (isSensorActive) {
+    //   startSensor();
+    //   setHoldTimeout(setTimeout(() => {
+    //     setIsSensorActive(false);
+    //     showNotification('Held for 10 seconds without shaking.');
+    //   }, 10000));
+    // } else {
+    //   stopSensor();
+    //   clearTimeout(holdTimeout);
+    // }
+    if(isSensorActive){
       startSensor();
-    } else {
+    }else{
       stopSensor();
     }
-
     return () => {
       stopSensor();
+      // clearTimeout(holdTimeout);
     };
   }, [isSensorActive]);
 
   const toggleSensor = () => {
     setIsSensorActive((prevIsSensorActive) => !prevIsSensorActive);
   };
+  const showNotification = (message) => {
+    toast.warn(message, {
+      position: "top-center",
+      autoClose: false,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      });
+  };
   return (
     <>
-      <div>
-        <h1>Accelerometer and Gyroscope Example</h1>
+      <div className="container">
+        <h1>Examine Motion</h1>
         <div>
-          <p>Acceleration:</p><br />
+          <p>Acceleration:</p>
+          <br />
           <p>X: {acceleration.x}</p>
           <br />
           <p>Y: {acceleration.y}</p>
@@ -99,9 +121,23 @@ function App() {
           <p>Gamma: {gyroscope.gamma}</p>
         </div>
 
-        <button onClick={toggleSensor}>
-          {isSensorActive ? "Stop Sensor" : "Start Sensor"}
-        </button>
+        <div className="button" onClick={toggleSensor}>
+          {isSensorActive ? (
+            <img className="icon" src={pauseIcon} alt="Pause Icon" />
+          ) : (
+            <img className="icon" src={playIcon} alt="Play Icon" />
+          )}
+        </div>
+        <ToastContainer
+          position="top-center"
+          autoClose={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          theme="light"
+        />
       </div>
     </>
   );
